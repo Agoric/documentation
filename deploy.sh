a4728dc91c77e0a52b418c7005f3595d05044962
Barbara Liau <barbara@liau.io>
2019-10-27 13:50:45 -0600

    Update deploy.sh script

--- deploy.sh
+++ deploy.sh
@@ -10,7 +10,7 @@ npm run docs:build
 cd ertp/.vuepress/dist
 
 # if you are deploying to a custom domain
-# echo 'www.example.com' > CNAME
+echo 'agoric.com/Documentation' > CNAME
 
 git init
 git add -A
