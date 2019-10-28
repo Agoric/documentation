04fe4fb84a91f76f1067233b7b811b38e54fd5c9
Barbara Liau <barbara@liau.io>
2019-10-28 13:45:59 -0600

    Reorganize nav and sidebar into own files

---
+++ main/.vuepress/themeConfig/nav.js
@@ -0,0 +1,71 @@
+/* --- NAVBAR (top) --- */
+// NOTES:
+// Internal links: Must have a corresponding folder with a README.md file
+// Links must be absolute with trailing slash '/guide/'
+// Trailing slash implies it is looking for a .md file
+
+module.exports = [
+  {
+    text: 'ERTP   ', // spaces to add some distance to next link
+    ariaLabel: 'ERTP Menu',
+    items: [
+      {
+        text: 'Guide',
+        ariaLabel: 'ERTP Guide Link',
+        link: '/ertp/guide/'
+      },
+      {
+        text: 'API',
+        ariaLabel: 'ERTP API Link',
+        link: '/ertp/api/'
+      },
+      {
+        text: 'Github',
+        ariaLabel: 'ERTP Github Link',
+        link: 'https://github.com/Agoric/ERTP'
+      }
+    ]
+  },
+  {
+    text: 'Zoe',
+    ariaLabel: 'Zoe Menu',
+    items: [
+      {
+        text: 'Guide',
+        ariaLabel: 'Zoe Guide',
+        link: '/zoe/guide/'
+      },
+      {
+        text: 'API',
+        ariaLabel: 'Zoe API',
+        link: '/zoe/api/'
+      },
+      {
+        text: 'Github',
+        ariaLabel: 'Zoe Github Link',
+        link: 'https://github.com/Agoric/Zoe'
+      }
+    ],
+  },
+  {
+    text: 'Learn More',
+    ariaLabel: 'Learn More Menu',
+    items: [
+      {
+        text: 'Contributing',
+        ariaLabel: 'Contributing',
+        link: '/miscellaneous/contributing'
+      },
+      {
+        text: 'Agoric',
+        ariaLabel: 'Agoric Homepage Link',
+        link: 'https://agoric.com/'
+      }
+    ]
+  },
+  {
+    text: 'Github',
+    ariaLabel: 'Agoric Github Link',
+    link: 'https://github.com/Agoric/'
+  }
+]
