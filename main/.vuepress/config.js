291550758db4d6db74c81b0fe0a464ab05a5103d
Barbara Liau <barbara@liau.io>
2019-10-28 19:34:40 -0600

    Add different sidebar nav for ERTP and Zoe views

--- main/.vuepress/config.js
+++ main/.vuepress/config.js
@@ -1,5 +1,4 @@
 const nav = require('./themeConfig/nav')
-const sidebar = require('./themeConfig/sidebar')
 
 module.exports = {
   /* --- FOR DEPLOYEMENT TO GITHUB PAGES--- */
@@ -13,7 +12,76 @@ module.exports = {
     /* --- NAVBAR (top) --- */
     nav,
     /* --- SIDEBAR --- */
-    sidebar
+    // This configuration displays different sidebars for different sections of
+    // content. Pages must be organized into directories for each desired
+    // section
+
+    // NOTES:
+    // Internal links: Must have a corresponding folder with a README.md file
+    // Links must be absolute with trailing slash '/guide/'
+    // Trailing slash implies it is looking for a .md file
+    sidebar: {
+      '/zoe/': [
+        {
+          title: 'Zoe',
+          path: '/zoe/guide/',
+          collapsable: false,
+          sideBarDepth: 5,
+          children: [
+            '/zoe/guide/',
+            '/zoe/guide/offer-safety'
+          ]
+        },
+        {
+          title: 'Zoe Contracts',
+          path: '/zoe/guide/contracts/',
+          collapsable: false,
+          sideBarDepth: 3,
+          children: [
+            '/zoe/guide/contracts/',
+            '/zoe/guide/contracts/autoswap',
+            '/zoe/guide/contracts/public-auction',
+            '/zoe/guide/contracts/public-swap'
+          ]
+        },
+        {
+          title: 'Zoe API',
+          path: '/zoe/api/',
+          collapsable: false
+        }
+      ],
+
+      '/ertp/': [
+        {
+          title: 'ERTP Guide',
+          path: '/ertp/guide/',
+          collapsable: false,
+          sideBarDepth: 3,
+          children: [
+            '/ertp/guide/',
+            '/ertp/guide/getting-started',
+            '/ertp/guide/assays',
+            '/ertp/guide/default-configuration',
+            '/ertp/guide/contract-hosts'
+          ]
+        },
+        {
+          title: 'ERTP API',
+          path: '/ertp/api/',
+          collapsable: false,
+          sideBarDepth: 3,
+          children: [
+            '/ertp/api/',
+            '/ertp/api/mint',
+            {
+              title: 'DescOps',
+              path: '/ertp/api/descOps'
+            }
+          ]
+        }
+      ]
+    }
+
     /* --- SEARCH --- */
     // Comes with built-in search functionality which builds its index from the
     // h1, h2, and h3 headers
