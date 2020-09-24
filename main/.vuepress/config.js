63595dac2719de913b104826379e86b0adb7c669
Kate Sills <kate@agoric.com>
2020-09-24 15:18:21 -0700

    chore: change repo to docsRepo to not add link to Github

--- main/.vuepress/config.js
+++ main/.vuepress/config.js
@@ -224,7 +224,7 @@ module.exports = {
         }
       ],
     },
-    repo: 'Agoric/documentation',
+    docsRepo: 'Agoric/documentation',
     // if your docs are not at the root of the repo:
     docsDir: 'main',
     // defaults to false, set to true to enable
