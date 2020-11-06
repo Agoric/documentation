c850a557c4adb9458bd31be8c03823d079d41528
Kate Sills <kate@agoric.com>
2020-11-05 22:13:40 -0800

    chore: add oracle documentation (#375)
    * chore: add oracle documentation
    
    * chore: add to list of contracts

--- main/.vuepress/config.js
+++ main/.vuepress/config.js
@@ -206,13 +206,14 @@ module.exports = {
           collapsible: false,
           sideBarDepth: 3,
           children: [
+            '/zoe/guide/contracts/oracle',
             '/zoe/guide/contracts/atomic-swap',
             '/zoe/guide/contracts/autoswap',
             '/zoe/guide/contracts/barter-exchange',
             '/zoe/guide/contracts/covered-call',
             '/zoe/guide/contracts/multipoolAutoswap',
             '/zoe/guide/contracts/second-price-auction',
-            '/zoe/guide/contracts/simple-exchange'
+            '/zoe/guide/contracts/simple-exchange',
           ]
         },
         {
