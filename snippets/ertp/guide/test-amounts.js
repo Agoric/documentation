466b711907de2becd98ec20f8f66cfd9259a3b62
Tom Galloway <tyg@users.noreply.github.com>
2021-04-08 10:13:21 -0700

    Updated for amountMath changes

--- snippets/ertp/guide/test-amounts.js
+++ snippets/ertp/guide/test-amounts.js
@@ -1,18 +1,17 @@
 import { test } from '@agoric/zoe/tools/prepare-test-env-ava';
 
-import { makeIssuerKit } from '@agoric/ertp';
+import { amountMath, makeIssuerKit } from '@agoric/ertp';
 
 test('ertp guide amounts', async t => {
   const quatloosKit = makeIssuerKit('quatloos');
   const {
     brand: quatloosBrand,
-    amountMath: quatloosAmountMath,
     issuer: quatloosIssuer,
   } = quatloosKit;
   // #region manualMake
   const newAmount = { brand: quatloosBrand, value: 5n };
   // #endregion manualMake
-  t.deepEqual(newAmount, quatloosAmountMath.make(5));
+  t.deepEqual(newAmount, amountMath.make(quatloosBrand, 5n));
 
   const { brand, issuer } = quatloosKit;
   // #region isMyIssuer
@@ -33,7 +32,7 @@ test('ertp guide amounts', async t => {
 
   const brandToPurse = new Map();
   brandToPurse.set(brand, issuer.makeEmptyPurse());
-  const quatloos50 = quatloosAmountMath.make(50);
+  const quatloos50 = amountMath.make(quatloosBrand, 50n);
   const payment = quatloosKit.mint.mintPayment(quatloos50);
   // #region depositSomewhere
   const allegedBrand = payment.getAllegedBrand();
@@ -42,21 +41,16 @@ test('ertp guide amounts', async t => {
   // #endregion depositSomewhere
   t.deepEqual(depositAmount, quatloos50);
 
-  // #region amountMathGetBrand
-  const newBrand = quatloosAmountMath.getBrand();
-  // #endregion amountMathGetBrand
-  t.is(newBrand, quatloosBrand);
-
   // #region getValue
-  const quatloos123 = quatloosAmountMath.make(123);
+  const quatloos123 = amountMath.make(quatloosBrand, 123n);
   // returns 123
-  const value = quatloosAmountMath.getValue(quatloos123);
+  const value = amountMath.getValue(quatloosBrand, quatloos123);
   // #endregion getValue
 
   t.is(value, 123n);
 
   // #region make
-  const quatloos837 = quatloosAmountMath.make(837);
+  const quatloos837 = amountMath.make(quatloosBrand, 837n);
   // #endregion make
 
   t.deepEqual(quatloos837, { brand: quatloosBrand, value: 837n });
