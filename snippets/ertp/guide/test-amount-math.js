a97adca63a94eede998d67988098c1f6ba50c7fa
Tom Galloway <tyg@users.noreply.github.com>
2021-04-12 15:24:26 -0700

    Declare zcf

--- snippets/ertp/guide/test-amount-math.js
+++ snippets/ertp/guide/test-amount-math.js
@@ -157,6 +157,7 @@ test('ertp guide amountMath related', async t => {
 
 test('ertp guide zcf.getMathKind related', async t => {
   const { issuer, mint, brand } = makeIssuerKit('quatloos');
+  const { zcf } = await setupZCFTest(harden({ Quatloos: quatloosIssuer }));
   t.truthy(issuer);
   t.truthy(mint);
   t.truthy(brand);
