0448a900794deb473bf909329fb14ac450c6c4db
Michael FIG <mfig@agoric.com>
2021-04-24 02:25:00 -0600

    chore: fix the lint

--- snippets/ertp/guide/test-readme.js
+++ snippets/ertp/guide/test-readme.js
@@ -1,6 +1,10 @@
 import { test } from '@agoric/zoe/tools/prepare-test-env-ava';
 
 import { E } from '@agoric/eventual-send';
+
+// We need to disable this lint until @agoric/vats is released
+// and adopted in package.json.
+// eslint-disable-next-line import/no-extraneous-dependencies
 import { makeBoard } from '@agoric/vats/src/lib-board';
 
 import { amountMath, makeIssuerKit, MathKind } from '@agoric/ertp';
