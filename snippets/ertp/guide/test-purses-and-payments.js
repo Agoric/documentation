8b8c0e120b2090b9399a2fa8c2041fc597e8547d
Mark S. Miller <erights@users.noreply.github.com>
2021-12-25 12:26:06 -0800

    fix: escape brackets in regexp (#603)

--- snippets/ertp/guide/test-purses-and-payments.js
+++ snippets/ertp/guide/test-purses-and-payments.js
@@ -60,7 +60,7 @@ test('ertp guide purse deposit', async t => {
     // To cope with version skew, this pattern is current and or of the old
     // message and the new message. TODO: nce the old message no longer needs to
     // be supported, delete it, simplifying the pattern.
-    message: /(?:payment balance .* must equal amount .*)|(?:{"brand":"[Alleged: quatloos brand]","value":"\[100n\]"} - Must be equivalent to the literal pattern: {"brand":"\[Alleged: quatloos brand\]","value":"\[123n\]"})/,
+    message: /(?:payment balance .* must equal amount .*)|(?:{"brand":"\[Alleged: quatloos brand\]","value":"\[100n\]"} - Must be equivalent to the literal pattern: {"brand":"\[Alleged: quatloos brand\]","value":"\[123n\]"})/,
   });
   // #endregion deposit
 });
