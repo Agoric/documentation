0daf938d66f363d69860539a3eb6eb1a938d9418
Chris Hibbert <hibbert@agoric.com>
2022-12-05 15:03:35 -0800

    chore: integrate with changes for Zoe durability
    This change is backwards compatible.

--- snippets/test-intro-zoe.js
+++ snippets/test-intro-zoe.js
@@ -17,10 +17,7 @@ import bundleSource from '@endo/bundle-source';
 import { makeFakeVatAdmin } from '@agoric/zoe/tools/fakeVatAdmin.js';
 
 test('intro to zoe', async t => {
-  const { zoeService } = makeZoeKit(makeFakeVatAdmin().admin);
-  const feePurse = E(zoeService).makeFeePurse();
-  const zoe = E(zoeService).bindDefaultFeePurse(feePurse);
-
+  const { zoeService: zoe } = makeZoeKit(makeFakeVatAdmin().admin);
   const moolaKit = makeIssuerKit('moola');
   const simoleanKit = makeIssuerKit('simoleans');
 
@@ -132,9 +129,7 @@ test('intro to zoe', async t => {
 });
 
 test('intro to zoe - contract-format', async t => {
-  const { zoeService } = makeZoeKit(makeFakeVatAdmin().admin);
-  const feePurse = E(zoeService).makeFeePurse();
-  const zoe = E(zoeService).bindDefaultFeePurse(feePurse);
+  const { zoeService: zoe } = makeZoeKit(makeFakeVatAdmin().admin);
   const atomicSwapUrl = await importMetaResolve(
     './contract-format.js',
     import.meta.url,
