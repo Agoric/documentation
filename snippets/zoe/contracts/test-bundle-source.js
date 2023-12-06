4aa44ec67b928c7e8fbb77ca02510c68dae3e066
Dan Connolly <connolly@agoric.com>
2023-12-06 05:06:29 -0600

    test: test-bundle-source.js with regions

---
+++ snippets/zoe/contracts/test-bundle-source.js
@@ -0,0 +1,37 @@
+/**
+ * @file Test using bundleSource() on the contract.
+ */
+// @ts-check
+
+/* eslint-disable import/order -- https://github.com/endojs/endo/issues/1235 */
+import { test } from '../../prepare-test-env-ava.js';
+
+// #region bundleSourceImports
+import bundleSource from '@endo/bundle-source';
+import { createRequire } from 'module';
+// #endregion bundleSourceImports
+import { E } from '@endo/far';
+// #region importZoeForTest
+import { makeZoeKitForTest } from '@agoric/zoe/tools/setup-zoe.js';
+// #endregion importZoeForTest
+
+// #region contractPath
+const myRequire = createRequire(import.meta.url);
+const contractPath = myRequire.resolve(`../src/gameAssetContract.js`);
+// #endregion contractPath
+
+test('bundleSource() bundles the contract for use with zoe', async t => {
+  // #region testBundleSource
+  const bundle = await bundleSource(contractPath);
+  t.is(bundle.moduleFormat, 'endoZipBase64');
+  t.log(bundle.endoZipBase64Sha512);
+  t.true(bundle.endoZipBase64.length > 10_000);
+  // #endregion testBundleSource
+
+  // #region testInstall
+  const { zoeService: zoe } = await makeZoeKitForTest();
+  const installation = await E(zoe).install(bundle);
+  t.log(installation);
+  t.is(typeof installation, 'object');
+  // #endregion testInstall
+});
