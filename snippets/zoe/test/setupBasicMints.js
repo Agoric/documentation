a9eb89ba8730a07e64ee1f5fc906e297d05c56f4
Dan Connolly <connolly@agoric.com>
2024-02-05 18:24:23 -0600

    chore: copy setupBasicMints.js from agoric-sdk
    no longer available as @agoric/zoe/test/unitTests/setupBasicMints.js
    
    copied from ff9515b

---
+++ snippets/zoe/test/setupBasicMints.js
@@ -0,0 +1,48 @@
+import { makeIssuerKit, AmountMath } from '@agoric/ertp';
+import { makeScalarMapStore } from '@agoric/store';
+import { makeZoeForTest } from '../../tools/setup-zoe.js';
+import { makeFakeVatAdmin } from '../../tools/fakeVatAdmin.js';
+
+export const setup = () => {
+  const moolaKit = makeIssuerKit('moola');
+  const simoleanKit = makeIssuerKit('simoleans');
+  const bucksKit = makeIssuerKit('bucks');
+  const allIssuerKits = {
+    moola: moolaKit,
+    simoleans: simoleanKit,
+    bucks: bucksKit,
+  };
+  /** @type {MapStore<string, Brand<'nat'>>} */
+  const brands = makeScalarMapStore('brandName');
+
+  for (const k of Object.getOwnPropertyNames(allIssuerKits)) {
+    brands.init(k, allIssuerKits[k].brand);
+  }
+
+  const { admin: fakeVatAdmin, vatAdminState } = makeFakeVatAdmin();
+  const zoe = makeZoeForTest(fakeVatAdmin);
+
+  /** @type {<K extends AssetKind>(brand: Brand<K>) => (value: any) => Amount<K>} */
+  const makeSimpleMake = brand => value => AmountMath.make(brand, value);
+
+  const result = {
+    moolaIssuer: moolaKit.issuer,
+    moolaMint: moolaKit.mint,
+    moolaKit,
+    simoleanIssuer: simoleanKit.issuer,
+    simoleanMint: simoleanKit.mint,
+    simoleanKit,
+    bucksIssuer: bucksKit.issuer,
+    bucksMint: bucksKit.mint,
+    bucksKit,
+    brands,
+    moola: makeSimpleMake(moolaKit.brand),
+    simoleans: makeSimpleMake(simoleanKit.brand),
+    bucks: makeSimpleMake(bucksKit.brand),
+    zoe,
+    vatAdminState,
+  };
+  harden(result);
+  return result;
+};
+harden(setup);
