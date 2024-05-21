8c26bf910681067a5fa61729d7caaa69e445710d
Mudassir Shabbir <mudassir@agoric.com>
2024-05-21 17:43:58 +0500

    fix: removed unnecessary imports

--- snippets/zoe/contracts/test-zoe-01-hello.js
+++ snippets/zoe/contracts/test-zoe-01-hello.js
@@ -5,8 +5,6 @@ import { E } from '@endo/far';
 // eslint-disable-next-line import/no-unresolved -- https://github.com/avajs/ava/issues/2951
 import test from 'ava';
 // #endregion test-imports
-import * as state from '../src/02-state.js';
-import * as access from '../src/03-access.js';
 // #region test1
 import { start } from '../src/01-hello.js';
 
